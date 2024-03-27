3381c2bcc68e3273db8d3f88142d07966bed7a02
Владимир <87864771+TheDreamingMite@users.noreply.github.com>
2024-03-27 18:36:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import Control_servo as cs
+
+def main():
+    my_control = cs.Control_servo()
+    my_control.start()
+    my_control.run_key(delta = 15)
+
+if __name__ == "__main__":
+    main()
+#daaaaaasws

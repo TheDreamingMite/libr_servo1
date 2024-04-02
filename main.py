f0ca3d7d1761a8e422b8140b167a3bc00a06e75e
Владимир <87864771+TheDreamingMite@users.noreply.github.com>
2024-04-02 11:59:42 +0300

    main.py
    добавлен вызов метода для внешних кнопок

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import Control_servo as cs
 def main():
     my_control = cs.Control_servo()
     my_control.start()
-    my_control.run_key(delta = 15)
+    # my_control.run_key(delta = 15)
+    my_control.run_buttons(delta = 15)
 
 if __name__ == "__main__":
     main()

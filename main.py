58bc809de97c8975db465d120be2fb88a0f618ed
Владимир <87864771+TheDreamingMite@users.noreply.github.com>
2024-04-17 12:56:25 +0300

    Update main.py
    добавлен метод управления с джостика

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     my_control.start()
     # my_control.run_key(delta = 15)
     # my_control.run_buttons(delta = 15)
-    my_control.run_joystik(delta = 15):
+    my_control.run_joystik(delta = 15)
 
 if __name__ == "__main__":
     main()
